b494efc3d8f90b2f3dd2878905ed49c69bc48aba
Radek <radekk0002@gmail.com>
2021-03-01 01:40:05 +0100

    Added autocomplete, more animations, changed ForecastService and styles

---
+++ WeatherAppClient/Data/WeatherForecastService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WeatherAppClient.Data
+{
+    public class WeatherForecastService
+    {
+        public async Task<IShortWeatherForecast[]> GetDataAsync(double lat, double lon, string place)
+        {
+            HttpClient Http = new HttpClient();
+
+            var data = await Http.GetFromJsonAsync<JsonElement>($"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=hourly&units=metric&appid={WeatherAPIkey.APIkey}");
+        
+            DateTime date = DateTime.Now;
+
+            IShortWeatherForecast[] _forecasts = new IShortWeatherForecast[7];
+            for (int i = 0; i < _forecasts.Length; i++)
+            {
+                IShortWeatherForecast swf = new WeatherForecast();
+
+                swf.Date = date.AddDays(i);
+                swf.Wind = data.GetProperty("daily")[i].GetProperty("wind_speed").GetDouble();
+                swf.TemperatureC = Math.Round(data.GetProperty("daily")[i].GetProperty("temp").GetProperty("day").GetDouble(), 1);
+                swf.Summary = data.GetProperty("daily")[i].GetProperty("weather")[0].GetProperty("description").ToString();
+                swf.Icon = data.GetProperty("daily")[i].GetProperty("weather")[0].GetProperty("icon").ToString();
+                swf.Humidity = data.GetProperty("daily")[i].GetProperty("humidity").GetInt32();
+                _forecasts[i] = swf;
+            }
+
+            WeatherForecast wf = (WeatherForecast) (_forecasts[0]);
+
+            wf.Place = place;
+            wf.Pressure = data.GetProperty("daily")[0].GetProperty("pressure").GetInt32();
+            wf.Cloudiness = data.GetProperty("daily")[0].GetProperty("clouds").GetInt32();
+
+            _forecasts[0] = wf;
+
+            return _forecasts;
+        }
+        
+        
+    }
+}
---
+++ WeatherAppClient/wwwroot/javascript.js
@@ -0,0 +1,18 @@
+﻿function SetGoogle(inp, dotnetHelper){
+
+    const options = {
+        fields: ["geometry", "name"],
+    }
+    var autocomplete = new google.maps.places.Autocomplete(inp,options);
+    
+    google.maps.event.addListener(autocomplete, "place_changed", function(){
+        var near_place = autocomplete.getPlace();
+        var lat = near_place.geometry.location.lat();
+        var lng = near_place.geometry.location.lng();
+        var tmp = inp.value.split(',');
+        var name = tmp[0] + ", " + tmp[1];
+        
+        dotnetHelper.invokeMethodAsync("OnPlaceChanged", lat, lng, name);
+
+    })
+}
